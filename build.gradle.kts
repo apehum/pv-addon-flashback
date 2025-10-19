a8fa0d306aca4fd7d0df594ad6ee53de5d05e1dc
Apehum <apehum@proton.me>
2025-10-20 04:25:42 +0800

    build for 1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,15 @@ import java.net.URI
 
 plugins {
     kotlin("jvm") version "2.0.21"
-    id("fabric-loom") version "1.10-SNAPSHOT"
+    id("fabric-loom") version "1.11-SNAPSHOT"
     id("com.gradleup.shadow") version "8.3.5"
     id("su.plo.crowdin.plugin") version "1.1.0-SNAPSHOT"
 }
+
+val minecraftVersion = stonecutter.current.version.substringBefore('-')
+
+version = "${rootProject.version}+$minecraftVersion"
+base.archivesName.set(rootProject.name)
 
 repositories {
     mavenCentral()
@@ -23,13 +28,12 @@ repositories {
     }
 
     maven("https://repo.plasmoverse.com/releases")
-//    maven("https://repo.plasmoverse.com/snapshots")
     maven("https://jitpack.io")
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 loom {
-    accessWidenerPath = file("src/main/resources/pvaddonflashback.accesswidener")
+    accessWidenerPath = rootProject.file("src/main/resources/pvaddonflashback.accesswidener")
 }
 
 dependencies {
@@ -37,19 +41,19 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
 
-    minecraft("com.mojang:minecraft:1.21.1")
+    minecraft("com.mojang:minecraft:$minecraftVersion")
     mappings(loom.officialMojangMappings())
     modImplementation("net.fabricmc:fabric-loader:0.16.10")
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.115.3+1.21.1")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("deps.fabric_api")}")
 
-    modImplementation("maven.modrinth:plasmo-voice:fabric-1.21-2.1.4")
-    compileOnly("su.plo.voice.api:client:2.1.4")
-    compileOnly("su.plo.voice.api:server:2.1.4")
+    modImplementation("maven.modrinth:plasmo-voice:${property("deps.plasmo_voice")}")
+    compileOnly("su.plo.voice.api:client:2.1.6")
+    compileOnly("su.plo.voice.api:server:2.1.6")
 
-    modImplementation("maven.modrinth:flashback:0.20.1-fabric,1.21.1")
-    modImplementation("maven.modrinth:modmenu:11.0.3")
-    modImplementation("maven.modrinth:cloth-config:15.0.140+fabric")
+    modImplementation("maven.modrinth:flashback:${property("deps.flashback")}")
+    modImplementation("maven.modrinth:modmenu:${property("deps.modmenu")}")
+    modImplementation("maven.modrinth:cloth-config:${property("deps.cloth_config")}")
 }
 
 crowdin {
@@ -79,6 +83,16 @@ tasks {
     remapJar {
         dependsOn(shadowJar)
         inputFile.set(shadowJar.get().archiveFile)
+    }
+
+    val copyToRoot =
+        register<Copy>("copyToRoot") {
+            from(remapJar.get().archiveFile)
+            into(rootProject.layout.buildDirectory.dir("libs"))
+        }
+
+    build {
+        finalizedBy(copyToRoot)
     }
 
     processResources {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,17 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("dev.kikugie.stonecutter") version "0.7.10"
+}
+
+stonecutter {
+    centralScript = "build.gradle.kts"
+
+    create(rootProject) {
+        versions("1.21.1", "1.21.10")
+        vcsVersion = "1.21.1"
+    }
+}
+
 rootProject.name = "pv-addon-flashback"
